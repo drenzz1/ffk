994d5f4af9e6d12e0fa28018fe119f9fd097fbd1
drenzz1 <you@example.com>
2024-03-18 11:30:31 +0100

    init

---
+++ src/test/java/org/foo/ffk/FfkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.foo.ffk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
